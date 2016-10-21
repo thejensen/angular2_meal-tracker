9fb865c6b62b685a5b174c0636d3d57a4908bcd7
Sara Jay <jensese@MacBook-Pro-2.local>
2016-10-21 09:09:35 -0700

    Setup basic file structure to start this angular2 app about tracking meals.

---
+++ app/app.component.ts
@@ -0,0 +1,14 @@
+import {Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `
+  <div class="container">
+    <h1>Hey there, Gorgeous ;)</h1>
+  </div>
+  `
+})
+
+export class AppComponent {
+
+}
